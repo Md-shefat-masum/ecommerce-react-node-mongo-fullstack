8d93799933ec45a85a3791332fecdb0ef2416e9e
Md-shefat-masum <mshefat924@gmail.com>
2022-12-30 00:25:48 +0600

    db seeder

--- server-side/Models/userModel.js
+++ server-side/Models/userModel.js
@@ -25,5 +25,5 @@ const userSchema = mongoose.Schema({
     
 })
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model('users', userSchema);
 module.exports = userModel;
